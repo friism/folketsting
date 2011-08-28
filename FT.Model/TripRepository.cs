9510b01e139721858abc9c2c05856dbe20ed5d2d
Michael Friis <friism@gmail.com>
2011-08-28 15:21:38 -0700

    add trip repo

---
+++ FT.Model/TripRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using FT.DB;
+
+namespace FT.Model
+{
+	public class TripRepository : Repository
+	{
+		public CommitteeTrip GetTripById(int id)
+		{
+			return DB.CommitteeTrips.SingleOrDefault(x => x.CommitteeTripId == id);
+		}
+
+		public IQueryable<CommitteeTrip> GetTrips()
+		{
+			return DB.CommitteeTrips.Where(
+				x => x.Place != null && x.Place != "" &&
+					x.CommitteeTripDestinations.Any() &&
+					x.CommitteeTripParticipants.Any());
+		}
+	}
+}
