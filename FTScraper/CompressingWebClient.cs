654852d28c56f30f0aed94ecc685946d2464ae73
Michael Friis <friism@gmail.com>
2011-08-27 23:47:37 -0700

    add web clients

---
+++ FTScraper/CompressingWebClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net;
+
+namespace FT.Scraper
+{
+	public class CompressingWebClient : WebClient
+	{
+		protected override WebRequest GetWebRequest(Uri address)
+		{
+			HttpWebRequest request = base.GetWebRequest(address) as HttpWebRequest;
+			request.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+			return request;
+		}
+	}
+}
---
+++ FTScraper/CookieContainingWebClient.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+
+namespace FT.Scraper
+{
+	public class CookieContainingWebClient : CompressingWebClient
+	{
+		private System.Net.CookieContainer cookieContainer;
+		private string userAgent;
+		private int timeout;
+
+		public System.Net.CookieContainer CookieContainer
+		{
+			get { return cookieContainer; }
+			set { cookieContainer = value; }
+		}
+
+		public string UserAgent
+		{
+			get { return userAgent; }
+			set { userAgent = value; }
+		}
+
+		public int Timeout
+		{
+			get { return timeout; }
+			set { timeout = value; }
+		}
+
+		public CookieContainingWebClient()
+		{
+			timeout = -1;
+			cookieContainer = new CookieContainer();
+		}
+
+		protected override WebRequest GetWebRequest(Uri address)
+		{
+			WebRequest request = base.GetWebRequest(address);
+
+			if (request.GetType() == typeof(HttpWebRequest))
+			{
+				((HttpWebRequest)request).CookieContainer = cookieContainer;
+				((HttpWebRequest)request).UserAgent = userAgent;
+				((HttpWebRequest)request).Timeout = timeout;
+			}
+
+			return request;
+		}
+	}
+}
